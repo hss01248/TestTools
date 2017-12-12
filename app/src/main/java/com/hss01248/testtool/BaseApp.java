6b076021fcb3382ab1a82b056eff872600e9dc1b
hss01248 <huangss@silvrr.com>
2017-12-12 15:35:40 +0800

    测试log框架,最终选择Logger

---
+++ app/src/main/java/com/hss01248/testtool/BaseApp.java
@@ -0,0 +1,16 @@
+package com.hss01248.testtool;
+
+import android.app.Application;
+
+/**
+ * Created by huangshuisheng on 2017/12/12.
+ */
+
+public class BaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LogTest.init(this);
+    }
+}
