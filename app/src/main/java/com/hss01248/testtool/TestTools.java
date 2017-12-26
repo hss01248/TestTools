ec493c7a329d0cec52c4cfe642da8903daccba37
hss01248 <huangss@silvrr.com>
2017-12-26 16:34:09 +0800

    增加leakcanary

--- app/src/main/java/com/hss01248/testtool/TestTools.java
+++ app/src/main/java/com/hss01248/testtool/TestTools.java
@@ -13,6 +13,10 @@ import com.github.pedrovgs.lynx.LynxActivity;
 import com.github.pedrovgs.lynx.LynxConfig;
 import com.github.pedrovgs.lynx.LynxShakeDetector;
 import com.readystatesoftware.chuck.ChuckInterceptor;
+import com.squareup.leakcanary.AndroidExcludedRefs;
+import com.squareup.leakcanary.DisplayLeakService;
+import com.squareup.leakcanary.ExcludedRefs;
+import com.squareup.leakcanary.LeakCanary;
 
 import okhttp3.OkHttpClient;
 
@@ -37,7 +41,22 @@ public class TestTools {
             initStetho();
             MyLog.init(DEBUG);
             showLogcat();
+            initLeakcanary();
 
+    }
+
+    private static void initLeakcanary() {
+        ExcludedRefs excludedRefs = AndroidExcludedRefs.createAppDefaults()
+            .instanceField("android.view.inputmethod.InputMethodManager", "sInstance")
+            .instanceField("android.view.inputmethod.InputMethodManager", "mLastSrvView")
+            .instanceField("com.android.internal.policy.PhoneWindow$DecorView", "mContext")
+            .instanceField("android.support.v7.widget.SearchView$SearchAutoComplete", "mContext")
+            .build();
+
+        LeakCanary.refWatcher(context)
+            .listenerServiceClass(DisplayLeakService.class)
+            .excludedRefs(excludedRefs)
+            .buildAndInstall();
     }
 
     public static void openStickModeIfIsDebug(){
